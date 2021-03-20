c4ad1bf1730930e06e45f5fbafac05c7f5b62012
Konyaz <80565760+Konyaz@users.noreply.github.com>
2021-03-20 15:35:02 +0900

    add

---
+++ src/test/java/DragAndDrop.java
@@ -0,0 +1,22 @@
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDrop {
+    @Test
+    void successfulFillTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+
+
+
+
+
+
+    }
+}
